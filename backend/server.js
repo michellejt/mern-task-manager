0460bc8e62a93e39416ffafa4b943af6a472bd7e
michellejt <mtongas@gmail.com>
2023-05-14 00:40:54 +0930

    update server cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,13 @@ app.use(express.json())
 //gives access to the formdata x-www-form-urlencoded from the body of the request
 app.use(express.urlencoded({extended: false}))
 
-app.use(cors())
+app.use(
+    cors({
+    origin: ["http://localhost:3000/", "https://mjt-mern-task-app.netlify.app/"],
+    })
+)
+
+
 
 app.use("/api/tasks", taskRoutes)
 
